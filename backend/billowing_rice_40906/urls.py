097acc538306f3ed52137102abf5ba3af77c9850
Crowdbotics <team@crowdbotics.com>
2023-06-02 17:55:40 +0000

    Update admin title

--- backend/billowing_rice_40906/urls.py
+++ backend/billowing_rice_40906/urls.py
@@ -34,9 +34,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "Billowing Rice"
-admin.site.site_title = "Billowing Rice Admin Portal"
-admin.site.index_title = "Billowing Rice Admin"
+admin.site.site_header = "Dauntless Nation"
+admin.site.site_title = "Dauntless Nation Admin Portal"
+admin.site.index_title = "Dauntless Nation Admin"
 
 # swagger
 urlpatterns += [
