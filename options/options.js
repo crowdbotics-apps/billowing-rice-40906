fc2b76a64b740db4a52a674e4eef4a55da916249
phanikumar.perni@crowdbotics.com <phanikumar.perni@crowdbotics.com>
2023-06-08 17:07:05 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "billowing-rice-40906",
-  url: "https://billowing-rice-40906.botics.co",
-  api: "https://billowing-rice-40906.botics.co/api/v1"
-}
+  "name": "billowing-rice-40906",
+  "url": "https://billowing-rice-40906.botics.co",
+  "api": "https://billowing-rice-40906.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
